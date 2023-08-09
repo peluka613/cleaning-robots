3cfb1083f9f76b89f994f7ff39a32ecb9b15d51f
Carlos Valencia <Carlos.Valencia@coxautoinc.com>
2023-08-08 21:32:59 -0600

    Cleaning Robots: Create empty project

---
+++ src/main/java/com/poc/cleaningrobots/CleaningRobotsApplication.java
@@ -0,0 +1,13 @@
+package com.poc.cleaningrobots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningRobotsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleaningRobotsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poc/cleaningrobots/CleaningRobotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.cleaningrobots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaningRobotsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
